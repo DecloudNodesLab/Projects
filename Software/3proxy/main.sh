fe84f76a0e053ecde4abf3a9bf985acc6d473b04
Dimokus <Dimokus@mail.ru>
2023-03-02 14:52:00 +0300

    Update main.sh

--- Software/3proxy/main.sh
+++ Software/3proxy/main.sh
@@ -1,14 +1,22 @@
 #!/bin bash
-apt-get install -y sudo make build-essential git
+apt-get install -y wget make build-essential git nginx
+service nginx start
 git clone https://github.com/3proxy/3proxy.git
 cd 3proxy
-sudo make -f Makefile.Linux
+make -f Makefile.Linux
 cp ./bin/3proxy /usr/bin
-
-cat > /usr/bin/3proxy.cfg << EOF
-auth none
-log
-proxy -p80
+wget -O /usr/bin/3proxy.cfg https://raw.githubusercontent.com/DecloudNodesLab/Projects/main/Software/3proxy/3proxy.cfg
+IP=$(wget -qO- eth0.me)
+cat > /var/www/html/proxy.pac << EOF
+function  FindProxyForURL  ( url ,  host )  { 
+  // наши локальные URL из доменов ниже example.com не нуждаются в прокси: 
+  if  ( shExpMatch ( host ,  '*.slack.com*.telegram.org;*.discord.com;*.ru' ))  {
+    return 'DIRECT';
+  }
+  // All other requests go through port 8080 of proxy.example.com.
+  // should that fail to respond, go directly to the WWW:
+  return 'PROXY $IP:$PORT; DIRECT';
+}
 EOF
 
 3proxy /usr/bin/3proxy.cfg
