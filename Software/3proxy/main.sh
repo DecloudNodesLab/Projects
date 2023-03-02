ec1da7a4f45515d2982027f18798c9569d029f1d
Dimokus <Dimokus@mail.ru>
2023-03-02 15:53:01 +0300

    Update main.sh

--- Software/3proxy/main.sh
+++ Software/3proxy/main.sh
@@ -7,7 +7,7 @@ make -f Makefile.Linux
 cp ./bin/3proxy /usr/bin
 wget -O /usr/bin/3proxy.cfg https://raw.githubusercontent.com/DecloudNodesLab/Projects/main/Software/3proxy/3proxy.cfg
 IP=$(wget -qO- eth0.me)
-cat > /var/www/html/proxy.pac << EOF
+cat > /var/www/html/wpad.dat << EOF
 function  FindProxyForURL  ( url ,  host )  { 
 if  ( shExpMatch ( host ,  '*.slack.com*.telegram.org;*.discord.com;*.ru' ))  {
 return 'DIRECT';
