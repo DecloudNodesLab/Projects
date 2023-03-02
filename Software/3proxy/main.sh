7458e574791024da97f7181e79bdbb93e98a3948
Dimokus <Dimokus@mail.ru>
2023-03-02 14:57:42 +0300

    Update main.sh

--- Software/3proxy/main.sh
+++ Software/3proxy/main.sh
@@ -9,13 +9,10 @@ wget -O /usr/bin/3proxy.cfg https://raw.githubusercontent.com/DecloudNodesLab/Pr
 IP=$(wget -qO- eth0.me)
 cat > /var/www/html/proxy.pac << EOF
 function  FindProxyForURL  ( url ,  host )  { 
-  // наши локальные URL из доменов ниже example.com не нуждаются в прокси: 
-  if  ( shExpMatch ( host ,  '*.slack.com*.telegram.org;*.discord.com;*.ru' ))  {
-    return 'DIRECT';
-  }
-  // All other requests go through port 8080 of proxy.example.com.
-  // should that fail to respond, go directly to the WWW:
-  return 'PROXY $IP:$PORT; DIRECT';
+if  ( shExpMatch ( host ,  '*.slack.com*.telegram.org;*.discord.com;*.ru' ))  {
+return 'DIRECT';
+}
+return 'PROXY $IP:$PORT; DIRECT';
 }
 EOF
 
