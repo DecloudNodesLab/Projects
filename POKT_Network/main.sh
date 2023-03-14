7f3cb3b2485c4543d67c3544a3c4714d52bc98fc
Dimokus <Dimokus@mail.ru>
2023-03-14 20:57:24 +0300

    Update main.sh

--- POKT_Network/main.sh
+++ POKT_Network/main.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 TZ=Europe/Kiev && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt-get install -y wget gcc make git nvme-cli nano 
+apt-get install -y wget gcc make git nvme-cli nano unzip runit
+runsvdir -P /etc/service &
+curl -L https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o uplink_linux_amd64.zip
+unzip -o uplink_linux_amd64.zip
+install uplink /usr/bin/uplink
+
 if [[ -n $SSH_PASS ]]
 then
 apt-get install -y ssh 
@@ -49,5 +54,35 @@ pocket accounts get-validator
 sleep 3
 fi
 
+if [[ -n $CHAINS_LINK ]] && [[ -z $CHAINS_BASE64 ]]
+then
+wget -O /root/.pocket/config/chains.json $CHAINS_LINK
+fi
+if [[ -n $CHAINS_BASE64 ]] && [[ -z $CHAINS_LINK ]]
+then
+echo $CHAINS_BASE64 | base64 -d > /root/.pocket/config/chains.json
+fi
+sleep 2
+mkdir -p $HOME/.pocket/config && curl -o $HOME/.pocket/config/genesis.json $GENESIS_LINK
+sleep 2
+echo =Run node...= 
+mkdir -p /root/pocket/log    
+cat > /root/pocket/run <<EOF 
+#!/bin/bash
+exec 2>&1
+exec pocket start --seeds="$SEEDS" --$CHAIN
+EOF
+mkdir /tmp/log/
+cat > /root/pocket/log/run <<EOF 
+#!/bin/bash
+exec svlogd -tt /tmp/log/
+EOF
+chmod +x /root/pocket/log/run /root/pocket/run 
+ln -s /root/pocket /etc/service && ln -s /tmp/log/current /LOG
 
-sleep infinity
+sleep 20
+for ((;;))
+  do    
+    tail -100 /LOG && sleep 5m
+  done
+fi
