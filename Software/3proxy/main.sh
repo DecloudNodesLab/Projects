791f7298e2950791b73d3a66264451bf2d714578
Dimokus <Dimokus@mail.ru>
2023-05-19 02:18:26 +0300

    Update main.sh

--- Software/3proxy/main.sh
+++ Software/3proxy/main.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-apt-get install -y wget make build-essential git nginx
+apt-get install -y make build-essential git nginx
 service nginx start 
 git clone https://github.com/3proxy/3proxy.git
 cd 3proxy
 make -f Makefile.Linux
 cp ./bin/3proxy /usr/bin
-wget -O /usr/bin/3proxy.cfg https://raw.githubusercontent.com/DecloudNodesLab/Projects/main/Software/3proxy/3proxy.cfg
-IP=`curl ifconfig.me`
-sleep 3
-cat > /var/www/html/wpad.dat << EOF
-function  FindProxyForURL  ( url ,  host )  { 
-if  ( shExpMatch ( host ,  '*.slack.com;*.telegram.org;*.discord.com;*.ru;*.github.com;*.рф;*.google.com;*.youtube.com' ))  {
-return 'DIRECT';
-}
-return 'PROXY $IP:$PORT; DIRECT';
-}
-EOF
+curl -o /usr/bin/3proxy.cfg https://raw.githubusercontent.com/DecloudNodesLab/Projects/main/Software/3proxy/3proxy.cfg
 
 3proxy /usr/bin/3proxy.cfg
