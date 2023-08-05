0738132a1c97be9d44efc3f539f801bc89581c40
Dimokus <Dimokus@mail.ru>
2023-08-05 12:08:02 +0300

    Update provider.sh

--- CosmosSDK/Testnets/Lava/provider.sh
+++ CosmosSDK/Testnets/Lava/provider.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ -z $MNEMONIC ]] || [[ -z $CONFIG_LINK ]] || [[ -z $BINARY_LINK ]] || [[ -z $GEOLOCATION ]] ; then echo Service stopped! Check env MNEMONIC, CONFIG_LINK, BINARY_LINK and GEOLOCATION in your SDL! ; fi
-apt install -y tar lz4 runit
+apt install -y tar lz4 runit wget
 if [[ -n $SSH_PASS ]]; then apt install ssh -y; echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && (echo $SSH_PASS; echo $SSH_PASS) | passwd root && service ssh restart; fi
 wget -O /usr/bin/lavad $BINARY_LINK;
 (echo $MNEMONIC)|lavad keys add wallet --recover --keyring-backend test
@@ -22,4 +22,5 @@ exec svlogd -tt /tmp/log/
 EOF
 chmod +x /root/lavad/log/run /root/lavad/run 
 ln -s /root/lavad /etc/service && ln -s /tmp/log/current /LOG_NODE
+sleep 2m
 lavad rpcprovider /root/.lava/config/rpcprovider.yml --geolocation $GEOLOCATION --from wallet --keyring-backend test;
