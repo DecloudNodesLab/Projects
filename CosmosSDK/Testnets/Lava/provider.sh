6ff844950afbeb6e34fec18dbfa41023de52b232
Dimokus <Dimokus@mail.ru>
2023-08-05 17:44:25 +0300

    Update provider.sh

--- CosmosSDK/Testnets/Lava/provider.sh
+++ CosmosSDK/Testnets/Lava/provider.sh
@@ -22,6 +22,7 @@ echo == Complited ==
 mv /root/.lava/priv_validator_state.json.backup /root/.lava/data/priv_validator_state.json && STATE_SYNC=off
 fi
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" /root/.lava/config/config.toml
+sed -i.bak -e "s/^enable *=.*/enable = true/;" /root/.lava/config/config.toml
 wget -O /root/.lava/config/rpcprovider.yml $CONFIG_LINK 
 mkdir -p /root/lavad/log    
 cat > /root/lavad/run <<EOF 
